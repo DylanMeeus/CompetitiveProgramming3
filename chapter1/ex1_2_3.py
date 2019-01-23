bfea04b7216d2a9b2725a7ba22fad898e32d2fd1
dylan <meeusdylan@protonmail.com>
2019-01-23 23:19:23 +0100

    ex1.2.3#4

--- chapter1/ex1_2_3.py
+++ chapter1/ex1_2_3.py
@@ -2,10 +2,15 @@
 
 # Try to write these solutions as concise as possible.
 
-# 2. given a date, determine the day of the week.
+# 3. given a date, determine the day of the week.
 import datetime
-def dayOfWeek():
+import random
+def ex3():
     d = datetime.datetime.strptime("23/01/2019", "%d/%m/%Y")
     return d.strftime("%A")
 
+# 4. Given n random integers, print the distinct (unique) integers in sorted order
+def ex4():
+    rs = [random.randint(0,9) for x in range(100)]
+    print(set(rs)) # set will keep them sorted and unique ;-)
 
