3b179754b1348b6b8c908620e150c03c360b5ee9
dylan <meeusdylan@protonmail.com>
2019-02-13 01:15:46 +0100

    commit partials and tryouts

---
+++ UVa/chapter1/10141.py
@@ -0,0 +1,7 @@
+import sys
+
+if __name__ == '__main__':
+    lines = []
+    for line in sys.stdin:
+        lines.append(line)
+
---
+++ UVa/chapter1/11507.py
@@ -0,0 +1,13 @@
+import sys
+# bender b rodriguez!
+
+def determine(moves):
+    return moves
+
+if __name__ == '__main__':
+    lines = []
+    for line in sys.stdin:
+        lines.append(line)
+    for line in lines:
+        out = determine(line.split(" "))
+        print(out)
